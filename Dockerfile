26946d0044a86c4d6cd02a11fb4588c0c6befb52
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2025-02-16 14:55:50 -0300

    ajustes

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Usa a imagem base do JDK 17
+FROM openjdk:17-jdk-slim
+
+# Define o diretório de trabalho
+WORKDIR /app
+
+# Copia o JAR gerado pelo build
+COPY build/libs/*.jar app.jar
+
+# Exposição da porta do serviço
+EXPOSE 8080
+
+# Comando de execução da aplicação
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/kotlin/br/com/seteideias/kotlindeployecr/controller/HelloController.kt
+++ src/main/kotlin/br/com/seteideias/kotlindeployecr/controller/HelloController.kt
@@ -1,4 +1,15 @@
 package br.com.seteideias.kotlindeployecr.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
 class HelloController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello World"
+    }
 }
