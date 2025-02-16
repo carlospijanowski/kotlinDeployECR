ea3df6f9eb0bb5322c921c6b29b22980bbaf963a
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2025-02-16 14:54:48 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/seteideias/kotlindeployecr/KotlindeployecrApplication.kt
@@ -0,0 +1,11 @@
+package br.com.seteideias.kotlindeployecr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlindeployecrApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlindeployecrApplication>(*args)
+}
---
+++ src/main/kotlin/br/com/seteideias/kotlindeployecr/controller/HelloController.kt
@@ -0,0 +1,4 @@
+package br.com.seteideias.kotlindeployecr.controller
+
+class HelloController {
+}
